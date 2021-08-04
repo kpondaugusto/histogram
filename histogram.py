02c9175a0b3f3d14b5b5b80bbb31d08553096e8e
Kiera Pond <augusto-k@webmail.uwinnipeg.ca>
2021-08-04 13:36:42 -0500

    made pretty

--- histogram.py
+++ histogram.py
@@ -5,7 +5,14 @@ import matplotlib.pyplot as plt
 
 fn='data.txt'
 r=np.genfromtxt(fn,unpack=True)
-print(r)
-plt.hist(r,bins=[5.1,5.2,5.3,5.4,5.5])
+bins=[5.3,5.4,5.5,5.6]
+#print(r)
+plt.hist(r,bins=bins)
 plt.xlabel(r"R ($\Omega$)")
+def bins_labels(bins, **kwargs):
+    bin_w = (max(bins) - min(bins)) / (len(bins) - 1)
+    plt.xticks(np.arange(min(bins)+bin_w/2, max(bins), bin_w), bins, **kwargs)
+    plt.xlim(bins[0], bins[-1])
+
+bins_labels(bins)
 plt.show()
